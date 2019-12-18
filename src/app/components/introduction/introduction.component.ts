d5106d5084dc08888f618fbc98c9afcf880a7cb1
UrtziFM <urtzi.arana13@gmail.com>
2019-12-18 22:48:31 +0100

    changes in develop

--- src/app/components/introduction/introduction.component.ts
+++ src/app/components/introduction/introduction.component.ts
@@ -6,11 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./introduction.component.scss']
 })
 export class IntroductionComponent implements OnInit {
+  name: string;
+  surname: string;
+  age: number;
 
   constructor() {
+        this.name = 'Urtzi';
+        this.surname = 'Arana';
+        this.age = 38;
    }
 
   ngOnInit() {
+    console.log(this.name);
+    console.log(this.surname);
+    console.log(this.age);
   }
 
 }
