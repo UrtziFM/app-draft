241d34a92d886a147209588617d35e9430e2a5b1
UrtziFM <urtzi.arana13@gmail.com>
2019-12-22 13:06:35 +0100

    creating routes to new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { KnowledgeComponent } from './components/knowledge/knowledge.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '/', component: IntroductionComponent},
+  {path: '/Contact', component: ContactComponent },
+  {path: '/Welcome', component: WelcomeComponent},
+  {path: '/Knowledge', component: KnowledgeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroductionComponent } from './components/introduction/introduction.component';
 import { LanguagesComponent } from './components/languages/languages.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { KnowledgeComponent } from './components/knowledge/knowledge.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroductionComponent,
-    LanguagesComponent
+    LanguagesComponent,
+    ContactComponent,
+    WelcomeComponent,
+    KnowledgeComponent
   ],
   imports: [
     BrowserModule,
