72b123964471a7a39470ff4896182ce75a2aeaae
UrtziFM <urtzi.arana13@gmail.com>
2020-03-17 20:08:11 +0100

    I have added register endpoints and controllers

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -10,6 +10,45 @@ const jwtSecret = process.env.JWT_SECRET || 'development-secret'
 
 
 passport.use(
+  'register',
+  new LocalStrategy(
+    { usernameField: 'email', passwordField: 'password', session: false },
+    (email, password, done) => {
+      User.findOne({ email })
+        .then(user => {
+          if (user) {
+            const err = new Error('There is already an account registered with this email')
+            return done(err, null)
+          }
+
+          bcrypt.genSalt(12, (err, salts) => {
+            if (err) {
+              return done(err, null)
+            }
+
+            bcrypt.hash(password, salts, (hashError, encryptedPass) => {
+              if (hashError) {
+                return done(hashError, null)
+              }
+
+              const newUser = new User({
+                email,
+                password: encryptedPass
+              })
+
+              newUser
+                .save()
+                .then(user => done(null, user))
+                .catch(err => done(err, null))
+            })
+          })
+        })
+        .catch(err => done(err, null))
+    }
+  )
+)
+
+passport.use(
   'login',
   new LocalStrategy(
     { usernameField: 'email', passwordField: 'password', session: false },
--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,17 @@
 const passport = require('passport')
 
+const register = (req, res, next) => {
+  passport.authenticate('register', (err, user) => {
+    if (err || !user) {
+      const error = new Error(err ? err.message : 'There was an error creating the user')
+      return next(error)
+    }
+
+    login(req, res, next)
+  })(req, res, next)
+}
+
+
 const login = (req, res, next) => {
   passport.authenticate('login', (err, token) => {
     if (err || !token) {
@@ -16,6 +28,7 @@ const isLoggedIn = (req, res, next) => {
 }
 
 module.exports = {
+  register,
   login,
   isLoggedIn
 }
