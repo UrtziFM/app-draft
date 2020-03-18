9d4c26b36314782ba6b06a55a7ed307c4e6a27fa
UrtziFM <urtzi.arana13@gmail.com>
2020-03-18 19:02:39 +0100

    I have added auth service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { ContactComponent } from './components/contact/contact.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { KnowledgeComponent } from './components/knowledge/knowledge.component';
 import { HomeComponent } from './components/home/home.component';
 import { FormsListComponent } from './components/forms-list/forms-list.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component'
 
 
 
@@ -15,7 +19,10 @@ const routes: Routes = [
   {path: 'Welcome', component: WelcomeComponent},
   {path: 'Experience', component: ExperienceComponent},
   {path: 'Knowledge', component: KnowledgeComponent},
-  {path: 'Forms', component: FormsListComponent}
+  {path: 'Forms', redirectTo: 'Login', pathMatch: 'full', component: FormsListComponent},
+  {path: 'Login', component: LoginComponent },
+  {path: 'Register', component: RegisterComponent },
+  {path: 'Profile/:id', component: UserProfileComponent }
 ];
 
 @NgModule({
