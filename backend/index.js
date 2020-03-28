22aea87dc98c4d9f378f11885fe147d4056ab5bb
UrtziFM <urtzi.arana13@gmail.com>
2020-03-28 20:41:52 +0100

    get / added

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,9 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/', function(req, res){
+   res.redirect('/forms');
+});
 
 app.use('/forms', formRoute)
 
