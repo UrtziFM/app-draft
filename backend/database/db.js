50d84b00ae4a18d009d53113cc788ee839f9621c
UrtziFM <urtzi.arana13@gmail.com>
2020-03-28 11:11:12 +0100

    added mongolab

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/formdb'
+   // db: 'mongodb://localhost:27017/formdb'
+    db: 'mongodb://heroku_dplfdtfd:lq8mipo6fnr1r0sdq5t8cri64i@ds051665.mlab.com:51665/heroku_dplfdtfd'
   };
